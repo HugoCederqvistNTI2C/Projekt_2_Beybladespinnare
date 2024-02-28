8b4ce8d75feacfcd7ebf07d8299337b22e0e6ec1
HugoCederqvistNTI2C <hugo.cederqvist@elev.ga.ntig.se>
2024-02-28 21:41:13 +0100

    Update sketch_jan17a.ino

--- sketch_jan17a.ino
+++ sketch_jan17a.ino
@@ -1,7 +1,14 @@
-//Hejhejhej
+/*
 
+Filnamn: Beybladespinnare
+Författare: Hugo Cederqvist
+Datum: 2024-02-28
+Beskrivning: Detta programmet flyttar en relay motor och en pinne med hjälp av en servo motor vid ett knapptryck, sedan efter en lite kort 
+ögonblick startar den relay motorn. Nästa gång knappen trycks flyttas servomotorn tillbaka till första positionen för att sedan stänga av relay motorn.
 
-#include <Servo.h>  // Inkludera Servo biblioteket
+ */
+
+#include <Servo.h>  // Inkluderar Servo biblioteket
 
 const int button = 1;        // knappen är ansluten till pin 1 på Arduino-brädet
 const int relay_motor = 2;   // reläet är anslutet till pin 2 på Arduino-brädet
@@ -26,24 +33,26 @@ void setup() {
 }
 
 void loop() {
-  eButton = digitalRead(button);
-  lastButtonState = currentButtonState;
-  currentButtonState = digitalRead(button);
+  
+  eButton = digitalRead(button);// Läs av knappens status  
+  lastButtonState = currentButtonState;// Spara den aktuella knappens status som föregående status för nästa gång den trycks
+  currentButtonState = digitalRead(button);// Läs av den aktuella knappens status
 
-  if (lastButtonState == HIGH && currentButtonState == LOW) {
-    Serial.println("Knappen är nedtryckt");
-
-    // Ändra reläets tillstånd
-    servo();
+  
+  if (lastButtonState == HIGH && currentButtonState == LOW) {// Kontrollera om knappen trycktes ned (föregående status var HIGH och aktuell status är LOW)
+    
+    servo(); // Anropar funktionen servo
     delay(500);
 
-    motor_relay();
+    motor_relay(); //anropar funktionen motor_relay
 
 
     delay(100); // Förbikoppling av studs på knappen
   }
 }
-
+/*
+En funktion som stänger av motorn ifall den är på och sätter på motorn ifall den är avstängd.
+ */
 void motor_relay() {
   if (digitalRead(relay_motor) == HIGH) {
     digitalWrite(relay_motor, LOW);  // Stäng av reläet om det är på
@@ -54,7 +63,9 @@ void motor_relay() {
     Serial.println("Reläet är på");
   }
 }
-
+/*
+Denna funktion ändrar servomotorns position när den anropas och växlar riktningen för varje tryck på knappen.
+ */
 void servo() {
   if (servoState) {
     servoPosition += 30;
